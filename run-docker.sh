dbb723bae0528f168399591ccb320f55c063b1aa
phil <phil@bitbrain.co.za>
2021-08-08 17:30:07 +0200

    Updated

--- run-docker.sh
+++ run-docker.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-CONTAINER_ID=$(docker run -u zap -p 2375:2375 -d owasp/zap2docker-weekly zap.sh -daemon -port 2375 -host 127.0.0.1 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true)
+CONTAINER_ID=$(docker run --name zap -u zap -d -p 8090:8080 -i owasp/zap2docker-stable zap.sh -daemon -host 0.0.0.0 -port 8080 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true)
 
 # the target URL for ZAP to scan
 TARGET_URL=$1
 
-docker exec $CONTAINER_ID zap-cli -p 2375 status -t 120 && docker exec $CONTAINER_ID zap-cli -p 2375 open-url $TARGET_URL
+docker exec $CONTAINER_ID zap-cli -p 8080 status -t 120 && docker exec $CONTAINER_ID zap-cli -p 8080 open-url $TARGET_URL
 
-docker exec $CONTAINER_ID zap-cli -p 2375 spider $TARGET_URL
+docker exec $CONTAINER_ID zap-cli -p 8080 spider $TARGET_URL
 
-docker exec $CONTAINER_ID zap-cli -p 2375 active-scan -r $TARGET_URL
+docker exec $CONTAINER_ID zap-cli -p 8080 active-scan -r $TARGET_URL
 
-docker exec $CONTAINER_ID zap-cli -p 2375 alerts
+docker exec $CONTAINER_ID zap-cli -p 8080 alerts
 
 # docker logs [container ID or name]
 divider==================================================================
