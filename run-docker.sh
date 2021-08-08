959cd8f69f226702686733a837219a4b2d6fff38
phil <phil@bitbrain.co.za>
2021-08-08 17:50:18 +0200

    Add reporting

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CONTAINER_ID=$(docker run --name zap -u zap -d -p 8090:8080 -i owasp/zap2docker-stable zap.sh -daemon -host 0.0.0.0 -port 8080 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true)
+CONTAINER_ID=$(docker run --name zap -u zap -d -p 8090:8080 -i owasp/zap2docker-stable zap.sh -daemon -host 0.0.0.0 -port 8080 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -r result.html -x result.xml)
 
 # the target URL for ZAP to scan
 TARGET_URL=$1
